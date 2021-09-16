bdfc777bd83294a8d0a8808b5897826f72e57d9e
Dav1com <42521812+Dav1com@users.noreply.github.com>
2021-09-16 15:13:51 -0300

    parte 3: mapa de color

---
+++ graficos_3.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import BoundaryNorm
+from matplotlib.ticker import MaxNLocator
+import numpy as np
+
+f = open('out.txt', 'r')
+
+Z = []
+
+minX = int(1e18)
+maxX = 0
+minY = int(1e18)
+maxY = 0
+for line in f:
+    line = line.split(', ')
+    minX = min(minX, int(line[0]))
+    maxX = max(maxX, int(line[0]))
+    minY = min(minY, int(line[1]))
+    maxY = max(maxY, int(line[1]))
+    Z.append(abs(int(line[2]) - int(line[3])))
+
+x = np.arange(minX - 0.5, maxX + 1, 1)
+y = np.arange(minY - 0.5, maxY + 1, 1)
+Z = np.array(Z).reshape(maxX-minX+1, maxY-minY+1).transpose()
+
+plt.pcolormesh(x, y, Z)
+plt.show()
+
